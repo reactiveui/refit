b2f720447b4f8bb7e05eec2deca0d1c665e62ce0
Paul Betts <paul@paulbetts.org>
2013-07-23 16:47:38 -0400

    Split up RestService into a portable and implementation-specific section

---
+++ Refit/CastleRestService.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using Castle.DynamicProxy;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Refit
+{
+    class CastleRestService : IRestService
+    {
+        readonly ProxyGenerator proxyGen = new ProxyGenerator();
+        public T For<T>(HttpClient client)
+        {
+            var rb = new RequestBuilder(typeof(T));
+            return (T)proxyGen.CreateInterfaceProxyWithoutTarget(typeof(T), new RestServiceMethodMissing(rb, client));
+        }
+    }
+
+    class RestServiceMethodMissing : IInterceptor
+    {
+        readonly HttpClient client;
+        readonly Dictionary<string, Func<HttpClient, object[], object>> methodImpls;
+
+        public RestServiceMethodMissing(RequestBuilder requestBuilder, HttpClient client)
+        {
+            methodImpls = requestBuilder.InterfaceHttpMethods.ToDictionary(k => k, v => requestBuilder.BuildRestResultFuncForMethod(v));
+            this.client = client;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            if (!methodImpls.ContainsKey(invocation.Method.Name)) {
+                throw new NotImplementedException();
+            }
+
+            invocation.ReturnValue = methodImpls[invocation.Method.Name](client, invocation.Arguments);
+            Console.WriteLine(invocation.ReturnValue);
+        }
+    }
+}
--- Refit/RestService.cs
+++ Refit/RestService.cs
@@ -2,47 +2,33 @@ using System;
 using System.Net.Http;
 using System.Collections.Generic;
 using System.Linq;
-using Castle.DynamicProxy;
 using System.Threading.Tasks;
 using System.Threading;
 
 namespace Refit
 {
+    interface IRestService
+    {
+        T For<T>(HttpClient client);
+    }
+
     public static class RestService
     {
-        static readonly ProxyGenerator proxyGen = new ProxyGenerator();
+#if !PORTABLE
+        static internal IRestService platformRestService = new CastleRestService();
+#else
+        static internal IRestService platformRestService;
+#endif
+
         public static T For<T>(HttpClient client)
         {
-            var rb = new RequestBuilder(typeof(T));
-            return (T)proxyGen.CreateInterfaceProxyWithoutTarget(typeof(T), new RestServiceMethodMissing(rb, client));
+            return platformRestService.For<T>(client);
         }
 
         public static T For<T>(string hostUrl)
         {
             var client = new HttpClient() { BaseAddress = new Uri(hostUrl) };
             return RestService.For<T>(client);
-        }
-    }
-
-    class RestServiceMethodMissing : IInterceptor
-    {
-        readonly HttpClient client;
-        readonly Dictionary<string, Func<HttpClient, object[], object>> methodImpls;
-
-        public RestServiceMethodMissing(RequestBuilder requestBuilder, HttpClient client)
-        {
-            methodImpls = requestBuilder.InterfaceHttpMethods.ToDictionary(k => k, v => requestBuilder.BuildRestResultFuncForMethod(v));
-            this.client = client;
-        }
-
-        public void Intercept(IInvocation invocation)
-        {
-            if (!methodImpls.ContainsKey(invocation.Method.Name)) {
-                throw new NotImplementedException();
-            }
-
-            invocation.ReturnValue = methodImpls[invocation.Method.Name](client, invocation.Arguments);
-            Console.WriteLine(invocation.ReturnValue);
         }
     }
 }
