cc1f0453e465006a27609dc5caedf2f62dd9e5f1
Michael Buck <mhbuck@gmail.com>
2013-10-21 22:18:02 +0200

    Basic Method Structure.

---
+++ Refit-AspNet/RestMethodResolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Refit.AspNet
+{
+    public class RestMethodResolver : IRestMethodResolver
+    {
+        public Dictionary<string, RestMethodInfo> GetInterfaceRestMethodInfo(Type targetInterface)
+        {
+            return targetInterface.GetMethods()
+                .SelectMany(x =>
+                {
+                    var attrs = x.GetCustomAttributes(true);
+                    var hasRoutingAttribute = attrs.OfType<RouteAttribute>().Any();
+                    if (!hasRoutingAttribute) return Enumerable.Empty<RestMethodInfo>();
+
+                    return EnumerableEx.Return(buildRestMethodInfo(targetInterface, x));
+                })
+                .ToDictionary(k => k.Name, v => v);
+        }
+
+        public RestMethodInfo buildRestMethodInfo(Type targetInterface, MethodInfo methodInfo)
+        {
+            var restMethodInfo = new RestMethodInfo(targetInterface, methodInfo);
+
+            // Do more work.
+
+            return restMethodInfo;
+        }
+    }
+}
