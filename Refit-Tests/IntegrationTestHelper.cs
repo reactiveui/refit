1307a46f6bda144a2e1e6e1b352642664bd384f9
Paul Betts <paul@paulbetts.org>
2014-05-04 00:42:57 -0700

    Write some code to find uses of RestService.For<T>

---
+++ Refit-Tests/IntegrationTestHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Refit.Tests
+{
+    public static class IntegrationTestHelper
+    {
+        public static string GetPath(params string[] paths)
+        {
+            var ret = GetIntegrationTestRootDirectory();
+            return (new FileInfo(paths.Aggregate(ret, Path.Combine))).FullName;
+        }
+
+        public static string GetIntegrationTestRootDirectory()
+        {
+            // XXX: This is an evil hack, but it's okay for a unit test
+            // We can't use Assembly.Location because unit test runners love
+            // to move stuff to temp directories
+            var st = new StackFrame(true);
+            var di = new DirectoryInfo(Path.Combine(Path.GetDirectoryName(st.GetFileName())));
+
+            return di.FullName;
+        }
+    }
+}
---
+++ Refit-Tests/InterfaceStubGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using NUnit.Framework;
+
+namespace Refit.Tests
+{
+    // * Find all calls to RestService.For<T>, extract all T's
+    // * Search for all Interfaces, find the method definitions
+    // * Generate the data we need for the template based on interface method 
+    //   defn's
+    //
+    // What if the Interface is in another module? (fuck 'em)
+    // What if the Interface itself is Generic? (fuck 'em)
+    public class InterfaceStubGenerator
+    {
+        public List<string> FindInterfacesToGenerate(string path)
+        {
+            var tree = CSharpSyntaxTree.ParseFile(path);
+
+            var restServiceCalls = tree.GetRoot().DescendantNodes()
+                .OfType<MemberAccessExpressionSyntax>()
+                .Where(x => x.Expression is IdentifierNameSyntax &&
+                    ((IdentifierNameSyntax)x.Expression).Identifier.ValueText == "RestService" &&
+                    x.Name.Identifier.ValueText == "For");
+
+            return restServiceCalls
+                .SelectMany(x => ((GenericNameSyntax)x.Name).TypeArgumentList.Arguments)
+                .Select(x => ((IdentifierNameSyntax)x).Identifier.ValueText)
+                .Distinct()
+                .ToList();
+        }
+    }
+
+    public class InterfaceStubGeneratorTests
+    {
+        [Test]
+        public void FindInterfacesSmokeTest()
+        {
+            var input = IntegrationTestHelper.GetPath("RestService.cs");
+            var fixture = new InterfaceStubGenerator();
+
+            var result = fixture.FindInterfacesToGenerate(input);
+            Assert.AreEqual(2, result.Count);
+            Assert.True(result.Any(x => x == "IGitHubApi"));
+        }
+    }
+}
