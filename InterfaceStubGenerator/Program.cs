52ca23dfc662c91ffcdcd5d2cb7ff90a42f6e7d2
Paul Betts <paul@paulbetts.org>
2014-05-07 14:03:27 -0700

    Finish main() to generate the interface file

--- InterfaceStubGenerator/Program.cs
+++ InterfaceStubGenerator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,12 @@ namespace Refit.Generator
     {
         static void Main(string[] args)
         {
+            var generator = new InterfaceStubGenerator();
+            var target = new FileInfo(args[0]);
+            var files = args[1].Split(';').Select(x => new FileInfo(x)).ToArray();
+
+            var template = generator.GenerateInterfaceStubs(files.Select(x => x.FullName).ToArray());
+            File.WriteAllText(target.FullName, template);
         }
     }
 }
