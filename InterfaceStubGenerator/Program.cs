51510955c8a3dedc95f79d487b6963a75e3f4b8b
Paul Betts <paul@paulbetts.org>
2014-05-18 20:48:59 -0700

    Fix a dumb bug in the interface generator

--- InterfaceStubGenerator/Program.cs
+++ InterfaceStubGenerator/Program.cs
@@ -12,9 +12,16 @@ namespace Refit.Generator
     {
         static void Main(string[] args)
         {
+            // NB: @Compile passes us a list of files relative to the project
+            // directory - we're going to assume that the target is always in
+            // the same directory as the project file
             var generator = new InterfaceStubGenerator();
             var target = new FileInfo(args[0]);
-            var files = args[1].Split(';').Select(x => new FileInfo(x)).ToArray();
+            var targetDir = target.DirectoryName;
+
+            var files = args[1].Split(';')
+                .Select(x => new FileInfo(Path.Combine(targetDir, x)))
+                .ToArray();
 
             var template = generator.GenerateInterfaceStubs(files.Select(x => x.FullName).ToArray());
             File.WriteAllText(target.FullName, template);
