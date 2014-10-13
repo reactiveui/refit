db02a2e5acc2c1cbd8445b2202eccc489651e346
Paul Betts <paul@paulbetts.org>
2014-10-13 10:04:46 -0700

    ToLowerInvariant compare

--- Refit/HtmlEncoder.cs
+++ Refit/HtmlEncoder.cs
@@ -931,13 +931,20 @@ namespace System.Web
     // Fine, I'll just make my own
     static class Uri
     {
-        static readonly Dictionary<char, int> hexDigits
-            = "0123456789abcdef".Select((c, i) => new {c, i})
-                .ToDictionary(x => x.c, c => c.i);
+        static readonly Dictionary<char, int> hexDigits;
+
+        static Uri()
+        {
+            hexDigits = new[] {
+                '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
+            }.Zip(Enumerable.Range(0, 0xf), (c, i) => new { c, i })
+             .ToDictionary(k => k.c, v => v.i);
+        }
 
         public static bool IsHexDigit(char c)
         {
-            return hexDigits.ContainsKey(c);
+            return hexDigits.ContainsKey(Char.ToLowerInvariant(c));
         }
 
         public static int FromHex(char digit)
