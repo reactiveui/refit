99f50496579ba17f1cca28034390a82ba50ef303
Bennor McCarthy <bennor@gmail.com>
2014-10-26 20:18:59 +1100

    Allow completely empty URL template
    This is needed (in conjunction with #40) for generic interfaces to be of
    any use at all.

--- Refit/RequestBuilderImplementation.cs
+++ Refit/RequestBuilderImplementation.cs
@@ -352,8 +352,10 @@ namespace Refit
             }
         }
 
-        void verifyUrlPathIsSane(string relativePath)
-        {
+        void verifyUrlPathIsSane(string relativePath) {
+            if (relativePath == "")
+                return;
+
             if (!relativePath.StartsWith("/")) {
                 goto bogusPath;
             }
