06d5e01f55c6f480a69a562f458f67a0d877c529
Oren Novotny <oren@novotny.org>
2014-12-19 15:04:55 -0500

    Reproduce bug #101

---
+++ Refit-Tests/AConflictingApi2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+using Refit; // InterfaceStubGenerator looks for this
+using Refit.Tests.Api2;
+
+
+
+namespace Refit.Tests.Api2
+{
+    public class TypeConflict
+    {
+        public string Name { get; set; }
+    }
+
+}
+
+namespace Refit.Tests
+{
+
+
+    public interface IAnotherApi
+    {
+        [Get("/foo")]
+        Task FooBar(TypeConflict user);
+    }
+
+}
