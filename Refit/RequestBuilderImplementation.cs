685735daf34255eea77994783f1a4ce1b35496ce
Michael Lamers <devmil@posteo.de>
2017-05-02 23:19:20 +0200

    (re-)added comments

--- Refit/RequestBuilderImplementation.cs
+++ Refit/RequestBuilderImplementation.cs
@@ -787,19 +787,25 @@ namespace Refit
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             HttpResponseMessage result;
+            // See if the request has an authorize header
             if (request.Headers.Authorization != null)
             {
                 var token = await getToken().ConfigureAwait(false);
 
+                //1st try using the provided token
                 result = await SendWithTokenAsync(request, cancellationToken, token).ConfigureAwait(false);
+                //if we got a 401 and a getRefreshedToken callback is set up
                 if (result.StatusCode == System.Net.HttpStatusCode.Unauthorized && getRefreshedToken != null)
                 {
+                    //use it to get a refreshed token
                     var refreshedToken = await getRefreshedToken().ConfigureAwait(false);
+                    //and retry using it. When this call still results in a 401 an exception is thrown later (as expected)
                     result = await SendWithTokenAsync(request, cancellationToken, refreshedToken).ConfigureAwait(false);
                 }
             }
             else
             {
+                //when we don't have a authorization header then proceed without any extra logic
                 result = await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
             }
             return result;
@@ -807,6 +813,7 @@ namespace Refit
 
         private async Task<HttpResponseMessage> SendWithTokenAsync(HttpRequestMessage request, CancellationToken cancellationToken, string token)
         {
+            // See if the request has an authorize header
             var auth = request.Headers.Authorization;
             if (auth != null)
             {
