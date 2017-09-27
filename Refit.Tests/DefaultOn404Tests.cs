455ce6d50cbab95099fcefa51e0b3a10163b5076
Natan Vivo <nvivo64@gmail.com>
2017-09-27 12:15:34 -0300

    Adds a generic way to filter unsuccessful responses and an attribute to return the default value when the server returns 404.

---
+++ Refit.Tests/DefaultOn404Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
+using Xunit;
+
+namespace Refit.Tests
+{
+    public class DefaultOn404Tests
+    {
+        private DefaultOn404Api fixture;
+
+        public DefaultOn404Tests()
+        {
+            var mockHttp = new MockHttpMessageHandler();
+
+            var settings = new RefitSettings
+            {
+                HttpMessageHandlerFactory = () => mockHttp
+            };
+
+            mockHttp.When(HttpMethod.Get, "https://404/object").Respond(HttpStatusCode.NotFound, null, string.Empty);
+            mockHttp.When(HttpMethod.Get, "https://404/int").Respond(HttpStatusCode.NotFound, null, string.Empty);
+
+            fixture = RestService.For<DefaultOn404Api>("https://404", settings);
+        }
+
+        [Fact]
+        public async Task ReferenceTypesReturnsNull()
+        {
+            var result = await fixture.GetObjectAsync();
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task ValueTypesReturnsDefaultValue()
+        {
+            var result = await fixture.GetInt32Async();
+
+            Assert.Equal(default(int), result);
+        }
+    }
+}
