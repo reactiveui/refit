ab96451d1c427c08d6207d151471950d6f7e9dd4
Sean Killeen <SeanKilleen@gmail.com>
2018-03-10 08:55:01 -0500

    Move test class from being nested class
    To appease the code auto-generation gods

--- Refit.Tests/AliasAsResponseTests.cs
+++ Refit.Tests/AliasAsResponseTests.cs
@@ -10,17 +10,17 @@ using Refit; // for the code gen
 using Xunit;
 
 namespace Refit.Tests {
+    public class TestAliasObject
+    {
+        [AliasAs("FIELD_WE_SHOULD_SHORTEN_WITH_ALIAS_AS")]
+        public string ShortNameForAlias { get; set; }
+
+        [JsonProperty(PropertyName = "FIELD_WE_SHOULD_SHORTEN_WITH_JSON_PROPERTY")]
+        public string ShortNameForJsonProperty { get; set; }
+    }
+
     public class AliasAsResponseTests
     {
-        public class TestAliasObject
-        {
-            [AliasAs("FIELD_WE_SHOULD_SHORTEN_WITH_ALIAS_AS")]
-            public string ShortNameForAlias { get; set; }
-
-            [JsonProperty(PropertyName = "FIELD_WE_SHOULD_SHORTEN_WITH_JSON_PROPERTY")]
-            public string ShortNameForJsonProperty { get; set; }
-        }
-
         public interface IMyAliasService
         {
             [Get("/aliasTest")]
