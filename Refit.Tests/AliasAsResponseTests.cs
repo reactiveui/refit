fc6e990426a5366c89dfd3161da1c234b19d81a3
Sean Killeen <SeanKilleen@gmail.com>
2018-03-10 08:35:14 -0500

    add test alias object

--- Refit.Tests/AliasAsResponseTests.cs
+++ Refit.Tests/AliasAsResponseTests.cs
@@ -4,6 +4,15 @@ using Newtonsoft.Json;
 namespace Refit.Tests {
     public class AliasAsResponseTests
     {
+        public class TestAliasObject
+        {
+            [AliasAs("FIELD_WE_SHOULD_SHORTEN_WITH_ALIAS_AS")]
+            public string ShortNameForAlias { get; set; }
+
+            [JsonProperty(PropertyName = "FIELD_WE_SHOULD_SHORTEN_WITH_JSON_PROPERTY")]
+            public string ShortNameForJsonProperty { get; set; }
+        }
+
         public interface IMyAliasService
         {
             [Get("/testRoute")]
