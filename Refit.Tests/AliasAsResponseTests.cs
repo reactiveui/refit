3f307f58f9b3cd187e0e9f48e9cd44478befb8d8
Sean Killeen <SeanKilleen@gmail.com>
2018-03-10 08:34:58 -0500

    Add test file and interface

---
+++ Refit.Tests/AliasAsResponseTests.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Refit.Tests {
+    public class AliasAsResponseTests
+    {
+        public interface IMyAliasService
+        {
+            [Get("/testRoute")]
+            Task<TestAliasObject> GetTestObject();
+        }
+    }
+}
