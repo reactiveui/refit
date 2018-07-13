2c7571ef82f83f71d411b022f1092846e014f81d
Hans De Mulder <Styxxy@users.noreply.github.com>
2018-07-13 16:45:04 +0200

    fix #512 refit interface outside namespace

---
+++ Refit.Tests/IServiceWithoutNamespace.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+interface IServiceWithoutNamespace
+{
+    [Get("/")]
+    Task<string> GetRoot();
+}
