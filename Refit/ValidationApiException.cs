471764b15f3fd4f25100d0afd0a3f8c33a3afa6d
Fanie Reynders <fanie.reynders@t-mobile.nl>
2018-11-11 10:15:49 +0100

    added documentation

--- Refit/ValidationApiException.cs
+++ Refit/ValidationApiException.cs
@@ -2,6 +2,9 @@
 
 namespace Refit
 {
+    /// <summary>
+    /// An ApiException that is raised according to RFC 7807, which contains problem details for validation exceptions.
+    /// </summary>
     [Serializable]
     public class ValidationApiException : ApiException
     {
@@ -10,11 +13,20 @@ namespace Refit
             base(apiException.RequestMessage, apiException.HttpMethod, apiException.StatusCode, apiException.ReasonPhrase, apiException.Headers, apiException.RefitSettings)
         {
         }
+
+        /// <summary>
+        /// Creates a new instance of a ValidationException from an existing ApiException.
+        /// </summary>
+        /// <param name="exception">An instance of an ApiException to use to build a ValidationException.</param>
+        /// <returns>ValidationApiException</returns>
         public static ValidationApiException Create(ApiException exception)
         {
             return new ValidationApiException(exception);
         }
 
+        /// <summary>
+        /// The problem details of the RFC 7807 validation exception.
+        /// </summary>
         public new ProblemDetails Content => GetContentAs<ProblemDetails>();
 
     }
