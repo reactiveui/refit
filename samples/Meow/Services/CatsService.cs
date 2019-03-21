ad82d88e1f61b9a451a0417c99c1bca287487d73
Geoffrey Huntley <ghuntley@ghuntley.com>
2019-03-21 14:59:43 -0700

    added sample application

---
+++ samples/Meow/Services/CatsService.cs
@@ -0,0 +1,28 @@
+﻿using HttpClientDiagnostics;
+using Meow.Responses;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meow
+{
+    public class CatsService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ITheCatsAPI _theCatsApi;
+
+        public CatsService(string baseUrl)
+        {
+            _httpClient = new HttpClient(new HttpClientDiagnosticsHandler());
+            _theCatsApi = RestService.For<ITheCatsAPI>(baseUrl);
+        }
+
+        public async Task<BreedsResponse> Search(string breed)
+        {
+            return await _theCatsApi.Search(breed).ConfigureAwait(false);
+        }
+    }
+}
---
+++ samples/Meow/Services/ITheCatsAPI.cs
@@ -0,0 +1,17 @@
+﻿using Meow.Responses;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meow
+{
+    [Headers("x-api-key: redacted")]
+
+    public interface ITheCatsAPI
+    {
+        [Get("/v1/images/search")]
+        Task<BreedsResponse> Search([AliasAs("breed_id")] string breedIdentifier);
+    }
+}
