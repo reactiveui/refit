91170950f34d5fcd47ea7c71afb25d9d09fced3f
Geoffrey Huntley <ghuntley@ghuntley.com>
2019-09-26 13:22:43 -0400

    docs: sample now works again

--- samples/Meow/Services/ITheCatsAPI.cs
+++ samples/Meow/Services/ITheCatsAPI.cs
@@ -7,11 +7,10 @@ using System.Threading.Tasks;
 
 namespace Meow
 {
-    [Headers("x-api-key: redacted")]
-
+    [Headers("x-api-key: b95bfb30-55bc-4327-bb8b-35d740f70051")]
     public interface ITheCatsAPI
     {
         [Get("/v1/images/search")]
-        Task<BreedsResponse> Search([AliasAs("breed_id")] string breedIdentifier);
+        Task<IEnumerable<SearchResult>> Search([AliasAs("q")] string breedIdentifier);
     }
 }
