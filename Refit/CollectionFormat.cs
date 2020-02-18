267bdd9b0d4351db9f62707a9cf79f068ede21fe
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2020-02-19 01:03:14 +0200

    Allow specifying default CollectionFormat

---
+++ Refit/CollectionFormat.cs
@@ -0,0 +1,39 @@
+﻿namespace Refit
+{
+    /// <summary>
+    /// Collection format defined in https://swagger.io/docs/specification/2-0/describing-parameters/ 
+    /// </summary>
+    public enum CollectionFormat
+    {
+        /// <summary>
+        /// Values formatted with <see cref="RefitSettings.UrlParameterFormatter"/> or
+        /// <see cref="RefitSettings.FormUrlEncodedParameterFormatter"/>.
+        /// </summary>
+        RefitParameterFormatter,
+
+        /// <summary>
+        /// Comma-separated values
+        /// </summary>
+        Csv,
+
+        /// <summary>
+        /// Space-separated values
+        /// </summary>
+        Ssv,
+
+        /// <summary>
+        /// Tab-separated values
+        /// </summary>
+        Tsv,
+
+        /// <summary>
+        /// Pipe-separated values
+        /// </summary>
+        Pipes,
+
+        /// <summary>
+        /// Multiple parameter instances
+        /// </summary>
+        Multi
+    }
+}
