dd19299b5789757867c7cab9b1af86abf4209300
Sergio Pedri <sergio0694@live.com>
2020-03-22 17:17:38 +0100

    Removed empty line

--- Refit/Buffers/PooledBufferWriter.Stream.cs
+++ Refit/Buffers/PooledBufferWriter.Stream.cs
@@ -99,7 +99,6 @@ namespace Refit.Buffers
                 return destination.Length;
             }
 
-
             /// <inheritdoc/>
             public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
             {
