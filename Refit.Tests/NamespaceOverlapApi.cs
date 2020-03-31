071697c6396a1f318f739497d87aa05ed34c1740
Adam Gritt <agritt@optimalblue.com>
2020-03-31 14:41:20 -0500

    Added a unit test to show namespace overlap build failure

---
+++ Refit.Tests/NamespaceOverlapApi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Helper;
+using Refit.Tests.Common;
+// InterfaceStubGenerator looks for this
+using Refit;
+
+namespace Refit.Tests
+{
+    [SomeHelper]
+    public interface INamespaceOverlapApi
+    {
+        [Get("/")]
+        Task<SomeOtherType> SomeRequest();
+    }
+
+    public static class NamespaceOverlapApi
+    {
+        public static INamespaceOverlapApi Create()
+        {
+            return RestService.For<INamespaceOverlapApi>("http://somewhere.com");
+        }
+    }
+}
+
+namespace Common.Helper
+{
+    public class SomeHelperAttribute : Attribute { }
+}
+
+namespace Refit.Tests.Common
+{
+    public class SomeOtherType { }
+}
