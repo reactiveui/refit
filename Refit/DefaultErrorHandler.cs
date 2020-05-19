f17c40b3cbd32584739428a22b5a8cc4b2de5267
Anatoliy Mikhailov <yamaoto@mail.ru>
2020-05-19 17:04:22 +0900

    Add IErrorHandler

---
+++ Refit/DefaultErrorHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Refit
+{
+    public class DefaultErrorHandler: IErrorHandler
+    {
+        public virtual async Task<Exception> HandleErrorAsync(HttpRequestMessage message, HttpMethod httpMethod, HttpResponseMessage response,
+            RefitSettings refitSettings = null)
+        {
+            var exception = await ApiException.Create(response.RequestMessage, response.RequestMessage.Method, response, refitSettings)
+                .ConfigureAwait(false);
+            return exception;
+        }
+    }
+}
---
+++ Refit/IErrorHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refit
+{
+    public interface IErrorHandler
+    {
+        Task<Exception> HandleErrorAsync(HttpRequestMessage message, HttpMethod httpMethod, HttpResponseMessage response,
+            RefitSettings refitSettings = null);
+    }
+}
