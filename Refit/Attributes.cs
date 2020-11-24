301498d8e412749f37ccab0b74705a36c6bd22f3
James Tayler <jamest@unl.sh>
2020-11-24 22:33:16 +1300

    add docs for attribute and its member

--- Refit/Attributes.cs
+++ Refit/Attributes.cs
@@ -206,6 +206,11 @@ namespace Refit
         public string Header { get; }
     }
 
+    /// <summary>
+    /// Used to store the value in HttpRequestMessage.Properties for further processing in a custom DelegatingHandler.
+    /// If a string is supplied to the constructor then it will be used as the key in the HttpRequestMessage.Properties dictionary.
+    /// If no key is specified then the key will be defaulted to the name of the parameter.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Parameter)]
     public class PropertyAttribute : Attribute
     {
@@ -216,6 +221,9 @@ namespace Refit
             Key = key;
         }
 
+        /// <summary>
+        /// Specifies the key under which to store the value on the HttpRequestMessage.Properties dictionary.
+        /// </summary>
         public string Key { get; }
     }
 
