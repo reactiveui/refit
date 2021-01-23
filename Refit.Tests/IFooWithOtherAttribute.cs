239d468a66e3f37913f7944ee966384cbe975510
Claire Novotny <claire@novotny.org>
2021-01-23 08:53:19 -0500

    Add test class with other attribute on method

---
+++ Refit.Tests/IFooWithOtherAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Threading.Tasks;
+
+using Refit;
+
+interface IFooWithOtherAttribute
+{
+    [Get("/")]
+    Task GetRoot();
+
+    [DisplayName("/")]
+    Task PostRoot();
+}
