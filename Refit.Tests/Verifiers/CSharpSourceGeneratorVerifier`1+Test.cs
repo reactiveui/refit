6bb715841e3787148494701ca7b299dd4a7c3fba
Sam Harwell <Sam.Harwell@microsoft.com>
2021-01-27 16:26:24 -0800

    Set up project for testing InterfaceStubGenerator

---
+++ Refit.Tests/Verifiers/CSharpSourceGeneratorVerifier`1+Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+namespace Refit.Tests
+{
+    public static partial class CSharpSourceGeneratorVerifier<TSourceGenerator>
+        where TSourceGenerator : ISourceGenerator, new()
+    {
+        public class Test : CSharpSourceGeneratorTest<TSourceGenerator, XUnitVerifier>
+        {
+            public Test()
+            {
+                SolutionTransforms.Add((solution, projectId) =>
+                {
+                    var compilationOptions = solution.GetProject(projectId).CompilationOptions;
+                    compilationOptions = compilationOptions.WithSpecificDiagnosticOptions(
+                        compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
+                    solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
+
+                    return solution;
+                });
+            }
+        }
+    }
+}
