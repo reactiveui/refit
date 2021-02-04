b090e654b3e951e2845fce76f0c6bd11018c7b25
Claire Novotny <claire@novotny.org>
2021-02-04 11:05:27 -0500

    Remove using statement

--- InterfaceStubGenerator.Core/InterfaceStubGenerator.cs
+++ InterfaceStubGenerator.Core/InterfaceStubGenerator.cs
@@ -49,14 +49,13 @@ namespace Refit.Generator
             refitInternalNamespace = $"{refitInternalNamespace ?? string.Empty}RefitInternalGenerated";
 
             var attributeText = @$"
-using System;
 #pragma warning disable
 namespace {refitInternalNamespace}
 {{
     [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     [global::System.ComponentModel.EditorBrowsable(global::System.ComponentModel.EditorBrowsableState.Never)]
-    [AttributeUsage (AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate)]
-    sealed class PreserveAttribute : Attribute
+    [global::System.AttributeUsage (global::System.AttributeTargets.Class | global::System.AttributeTargets.Struct | global::System.AttributeTargets.Enum | global::System.AttributeTargets.Constructor | global::System.AttributeTargets.Method | global::System.AttributeTargets.Property | global::System.AttributeTargets.Field | global::System.AttributeTargets.Event | global::System.AttributeTargets.Interface | global::System.AttributeTargets.Delegate)]
+    sealed class PreserveAttribute : global::System.Attribute
     {{
         //
         // Fields
