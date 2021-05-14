c98d90f079e362f68499ec2dadfca93d0e1cdc44
James Tayler <james@tayler.io>
2021-05-14 21:00:14 +1200

    add custom attribute property provider

--- Refit.Tests/Extensions/Properties/PropertyProviderTests.cs
+++ Refit.Tests/Extensions/Properties/PropertyProviderTests.cs
@@ -8,12 +8,21 @@ using System.Threading.Tasks;
 using Refit.Extensions.Properties;
 
 using RichardSzalay.MockHttp;
+
 using Xunit;
 
 namespace Refit.Tests.Extensions.Properties
 {
     public class PropertyProviderTests
     {
+        [AttributeUsage(AttributeTargets.Method)]
+        private class RetryAttribute : Attribute
+        {
+            public int Times { get; }
+
+            public RetryAttribute(int times) => Times = times;
+        }
+
         public class MyDummyObject
         {
             protected bool Equals(MyDummyObject other)
@@ -26,14 +35,16 @@ namespace Refit.Tests.Extensions.Properties
                 if (ReferenceEquals(null, obj)) return false;
                 if (ReferenceEquals(this, obj)) return true;
                 if (obj.GetType() != this.GetType()) return false;
-                return Equals((MyDummyObject) obj);
+                return Equals((MyDummyObject)obj);
             }
 
             public string SomeValue { get; set; }
             public int AnotherValue { get; set; }
         }
+
         public interface IMyService
         {
+            [Retry(3)]
             [Get("/get-api-response-with-result")]
             Task<ApiResponse<MyDummyObject>> GetApiResponseWithResult([Property] string someProperty);
 
@@ -45,17 +56,12 @@ namespace Refit.Tests.Extensions.Properties
         public async Task GivenNullPropertyProvider_WhenInvokeRefit_Succeeds()
         {
             var propertyValue = "somePropertyValue";
-            var dummyObject = new MyDummyObject
-            {
-                SomeValue = "AValue",
-                AnotherValue = 1
-            };
+            var dummyObject = new MyDummyObject {SomeValue = "AValue", AnotherValue = 1};
 
             var handler = new MockHttpMessageHandler();
             var settings = new RefitSettings
             {
-                HttpMessageHandlerFactory = () => handler,
-                PropertyProviderFactory = null!
+                HttpMessageHandlerFactory = () => handler, PropertyProviderFactory = null!
             };
 
             handler.Expect(HttpMethod.Get, "http://api/get-with-result")
@@ -80,11 +86,7 @@ namespace Refit.Tests.Extensions.Properties
         public async Task GivenMethodInfoPropertyProvider_WhenInvokeRefit_MethodInfoPopulatedIntoProperties()
         {
             var propertyValue = "somePropertyValue";
-            var dummyObject = new MyDummyObject
-            {
-                SomeValue = "AValue",
-                AnotherValue = 1
-            };
+            var dummyObject = new MyDummyObject {SomeValue = "AValue", AnotherValue = 1};
             var methodInfoKey = "methodInfo";
 
             var handler = new MockHttpMessageHandler();
@@ -108,7 +110,8 @@ namespace Refit.Tests.Extensions.Properties
 
             Assert.Equal(dummyObject, result1);
             Assert.Equal(dummyObject, result2.Content);
-            Assert.IsAssignableFrom<MethodInfo>(result2.RequestMessage?.Properties[HttpRequestMessageOptions.MethodInfo]);
+            Assert.IsAssignableFrom<MethodInfo>(
+                result2.RequestMessage?.Properties[HttpRequestMessageOptions.MethodInfo]);
             Assert.Equal(2, result2.RequestMessage?.Properties.Count);
         }
 
@@ -116,11 +119,7 @@ namespace Refit.Tests.Extensions.Properties
         public async Task GivenPropertyProviderReturnsNull_WhenInvokeRefit_Success()
         {
             var propertyValue = "somePropertyValue";
-            var dummyObject = new MyDummyObject
-            {
-                SomeValue = "AValue",
-                AnotherValue = 1
-            };
+            var dummyObject = new MyDummyObject {SomeValue = "AValue", AnotherValue = 1};
             var methodInfoKey = "methodInfo";
 
             var handler = new MockHttpMessageHandler();
@@ -154,11 +153,7 @@ namespace Refit.Tests.Extensions.Properties
             var propertyValue = "somePropertyValue";
             var overwrittenPropertyValue = "aDifferentPropertyValue";
             var existingPropertyKey = "someProperty";
-            var dummyObject = new MyDummyObject
-            {
-                SomeValue = "AValue",
-                AnotherValue = 1
-            };
+            var dummyObject = new MyDummyObject {SomeValue = "AValue", AnotherValue = 1};
 
             var handler = new MockHttpMessageHandler();
             var settings = new RefitSettings
@@ -193,11 +188,7 @@ namespace Refit.Tests.Extensions.Properties
         {
             var propertyValue = "somePropertyValue";
             var existingPropertyKey = "someProperty";
-            var dummyObject = new MyDummyObject
-            {
-                SomeValue = "AValue",
-                AnotherValue = 1
-            };
+            var dummyObject = new MyDummyObject {SomeValue = "AValue", AnotherValue = 1};
             var tantrum = new Exception("I'm very unhappy about this...kaboom!");
 
             var handler = new MockHttpMessageHandler();
@@ -223,7 +214,45 @@ namespace Refit.Tests.Extensions.Properties
             Assert.Equal(dummyObject, result2.Content);
             Assert.Equal(2, result2.RequestMessage?.Properties.Count);
             Assert.Equal(propertyValue, result2.RequestMessage?.Properties[existingPropertyKey]);
-            Assert.Equal(tantrum, result2.RequestMessage?.Properties[HttpRequestMessageOptions.PropertyProviderException]);
+            Assert.Equal(tantrum,
+                result2.RequestMessage?.Properties[HttpRequestMessageOptions.PropertyProviderException]);
+        }
+
+        [Fact]
+        public async Task GivenCustomAttributePropertyProvider_WhenInvokeRefit_CustomAttributesPopulatedIntoProperties()
+        {
+            var propertyValue = "somePropertyValue";
+            var dummyObject = new MyDummyObject {SomeValue = "AValue", AnotherValue = 1};
+            var methodInfoKey = "methodInfo";
+
+            var handler = new MockHttpMessageHandler();
+            var settings = new RefitSettings
+            {
+                HttpMessageHandlerFactory = () => handler,
+                PropertyProviderFactory = PropertyProviderFactory.CustomAttributePropertyProvider
+            };
+
+            handler.Expect(HttpMethod.Get, "http://api/get-with-result")
+                .Respond(HttpStatusCode.OK, settings.ContentSerializer.ToHttpContent(dummyObject));
+            handler.Expect(HttpMethod.Get, "http://api/get-api-response-with-result")
+                .Respond(HttpStatusCode.OK, settings.ContentSerializer.ToHttpContent(dummyObject));
+
+            var fixture = RestService.For<IMyService>("http://api", settings);
+
+            var result1 = await fixture.GetWithResult(propertyValue);
+            var result2 = await fixture.GetApiResponseWithResult(propertyValue);
+
+            handler.VerifyNoOutstandingExpectation();
+
+            Assert.Equal(dummyObject, result1);
+            Assert.Equal(dummyObject, result2.Content);
+            Assert.Equal(2, result2.RequestMessage?.Properties.Count);
+#if NET5_0_OR_GREATER
+            RetryAttribute retryAttribute = default;
+            Assert.True(result2.RequestMessage.Options.TryGetValue(
+                new HttpRequestOptionsKey<RetryAttribute>(nameof(RetryAttribute)), out retryAttribute));
+            Assert.Equal(3, retryAttribute.Times);
+#endif
         }
     }
 }
--- Refit/Extensions/Properties/PropertyProviderFactory.cs
+++ Refit/Extensions/Properties/PropertyProviderFactory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Reflection;
 
 namespace Refit.Extensions.Properties
@@ -7,7 +8,7 @@ namespace Refit.Extensions.Properties
     public static class PropertyProviderFactory
     {
         /// <summary>
-        /// Populates the Refit interface type into HttpRequestMessage.Properties/Options
+        /// Populates the Refit interface type into the <see cref="HttpRequestMessage"/> properties
         /// </summary>
         public static IDictionary<string, object> DefaultPropertyProvider(MethodInfo methodInfo, Type targetType)
         {
@@ -16,14 +17,52 @@ namespace Refit.Extensions.Properties
             return properties;
         }
 
+        /// <summary>
+        /// Doesn't populate any properties into the <see cref="HttpRequestMessage"/> properties. Can be used to override the default behavior.
+        /// </summary>
         public static IDictionary<string, object> NullPropertyProvider(MethodInfo methodInfo, Type targetType)
         {
             return null;
         }
 
+        /// <summary>
+        /// Populates the <see cref="MethodInfo"/> of the currently executing method on the Refit interface into the <see cref="HttpRequestMessage"/> properties
+        /// </summary>
         public static IDictionary<string, object> MethodInfoPropertyProvider(MethodInfo methodInfo, Type targetType)
         {
             var properties = new Dictionary<string, object> {{HttpRequestMessageOptions.MethodInfo, methodInfo}};
+
+            return properties;
+        }
+
+        /// <summary>
+        /// Populates the any custom <see cref="Attribute"/>s present on the currently executing method on the Refit interface into the <see cref="HttpRequestMessage"/> properties
+        /// </summary>
+        public static IDictionary<string, object> CustomAttributePropertyProvider(MethodInfo methodInfo,
+            Type targetType)
+        {
+            var properties = new Dictionary<string, object>();
+            foreach (var attr in methodInfo.GetCustomAttributes())
+            {
+                if (attr is HttpMethodAttribute ||
+                    attr is AuthorizeAttribute ||
+                    attr is MultipartAttribute ||
+                    attr is PropertyAttribute ||
+                    attr is BodyAttribute ||
+                    attr is AliasAsAttribute ||
+                    attr is AttachmentNameAttribute ||
+                    attr is HeaderCollectionAttribute ||
+                    attr is HeaderAttribute ||
+                    attr is HeadersAttribute ||
+                    attr is PropertyAttribute ||
+                    attr is QueryAttribute ||
+                    attr is QueryUriFormatAttribute)
+                {
+                    continue;
+                }
+
+                properties[attr.GetType().Name] = attr;
+            }
 
             return properties;
         }
