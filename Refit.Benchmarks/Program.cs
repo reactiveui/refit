cd6307bfc5fb714c3b53b9a860057b23c60fc585
James Tayler <james@tayler.io>
2021-05-16 18:09:05 +1200

    run all end to end benchmarks if no args supplied, else run benchmarks for the args supplied

--- Refit.Benchmarks/Program.cs
+++ Refit.Benchmarks/Program.cs
@@ -1,9 +1,21 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+
+using BenchmarkDotNet.Running;
 
 namespace Refit.Benchmarks
 {
     class Program
     {
-        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+            }
+            else
+            {
+                BenchmarkRunner.Run<EndToEndBenchmark>();
+            }
+        }
     }
 }
