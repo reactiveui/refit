2c23aec026666ae64e1743b64cc742c310894506
Laurent Keusch <25180858+laurentksh@users.noreply.github.com>
2021-11-10 14:57:46 +0100

    -Added comments for ApiException. -Added comments for ApiResponse. -Added comments for most of the attributes. -Added comments for RestService.

--- Refit/ApiException.cs
+++ Refit/ApiException.cs
@@ -6,19 +6,60 @@ using System.Threading.Tasks;
 
 namespace Refit
 {
-
+    /// <summary>
+    /// Represents an error that occured while sending an API request.
+    /// </summary>
     [Serializable]
     public class ApiException : Exception
     {
+        /// <summary>
+        /// HTTP response status code.
+        /// </summary>
         public HttpStatusCode StatusCode { get; }
+
+        /// <summary>
+        /// The reason phrase which typically is sent by the server together with the status code.
+        /// </summary>
         public string? ReasonPhrase { get; }
+
+        /// <summary>
+        /// HTTP response headers.
+        /// </summary>
         public HttpResponseHeaders Headers { get; }
+
+        /// <summary>
+        /// The HTTP method used to send the request.
+        /// </summary>
         public HttpMethod HttpMethod { get; }
+
+        /// <summary>
+        /// The <see cref="System.Uri"/> used to send the HTTP request.
+        /// </summary>
         public Uri? Uri => RequestMessage.RequestUri;
+
+        /// <summary>
+        /// The HTTP Request message used to send the request.
+        /// </summary>
         public HttpRequestMessage RequestMessage { get; }
+
+        /// <summary>
+        /// HTTP response content headers as defined in RFC 2616.
+        /// </summary>
         public HttpContentHeaders? ContentHeaders { get; private set; }
+
+        /// <summary>
+        /// HTTP Response content as string.
+        /// </summary>
         public string? Content { get; private set; }
+
+        /// <summary>
+        /// Does the response have content?
+        /// </summary>
         public bool HasContent => !string.IsNullOrWhiteSpace(Content);
+
+        /// <summary>
+        /// Refit settings used to send the request.
+        /// </summary>
         public RefitSettings RefitSettings { get; }
 
         protected ApiException(HttpRequestMessage message, HttpMethod httpMethod, string? content, HttpStatusCode statusCode, string? reasonPhrase, HttpResponseHeaders headers, RefitSettings refitSettings, Exception? innerException = null) :
@@ -38,10 +79,24 @@ namespace Refit
             Content = content;
         }
 
+        /// <summary>
+        /// Get the deserialized response content as nullable <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">Type to deserialize the content to</typeparam>
+        /// <returns>The response content deserialized as <typeparamref name="T"/></returns>
         public async Task<T?> GetContentAsAsync<T>() => HasContent ?
                 await RefitSettings.ContentSerializer.FromHttpContentAsync<T>(new StringContent(Content!)).ConfigureAwait(false) :
                 default;
 
+        /// <summary>
+        /// Create an instance of <see cref="ApiException"/>.
+        /// </summary>
+        /// <param name="message">The HTTP Request message used to send the request.</param>
+        /// <param name="httpMethod">The HTTP method used to send the request.</param>
+        /// <param name="response">The HTTP Response message.</param>
+        /// <param name="refitSettings">Refit settings used to sent the request.</param>
+        /// <param name="innerException">Add an inner exception to the <see cref="ApiException"/>.</param>
+        /// <returns>A newly created <see cref="ApiException"/>.</returns>
 #pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
         public static Task<ApiException> Create(HttpRequestMessage message, HttpMethod httpMethod, HttpResponseMessage response, RefitSettings refitSettings, Exception? innerException = null)
 #pragma warning restore VSTHRD200 // Use "Async" suffix for async methods
@@ -50,6 +105,16 @@ namespace Refit
             return Create(exceptionMessage, message, httpMethod, response, refitSettings, innerException);
         }
 
+        /// <summary>
+        /// Create an instance of <see cref="ApiException"/> with a custom exception message.
+        /// </summary>
+        /// <param name="exceptionMessage">A custom exception message.</param>
+        /// <param name="message">The HTTP Request message used to send the request.</param>
+        /// <param name="httpMethod">The HTTP method used to send the request.</param>
+        /// <param name="response">The HTTP Response message.</param>
+        /// <param name="refitSettings">Refit settings used to sent the request.</param>
+        /// <param name="innerException">Add an inner exception to the <see cref="ApiException"/>.</param>
+        /// <returns>A newly created <see cref="ApiException"/>.</returns>
 #pragma warning disable VSTHRD200 // Use "Async" suffix for async methods
         public static async Task<ApiException> Create(string exceptionMessage, HttpRequestMessage message, HttpMethod httpMethod, HttpResponseMessage response, RefitSettings refitSettings, Exception? innerException = null)
 #pragma warning restore VSTHRD200 // Use "Async" suffix for async methods
