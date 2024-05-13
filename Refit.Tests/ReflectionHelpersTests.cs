1de9cae332dbc925716775cfa88b3fe6b6add926
Jesse Klaasse <jesse.klaasse@indicia.nl>
2024-05-13 09:12:43 +0200

    test: add unit tests for ReflectionHelpers

---
+++ Refit.Tests/ReflectionHelpersTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+using Refit;
+
+namespace Refit.Tests
+{
+    public class ReflectionHelpersTests
+    {
+        [Fact]
+        public void NullTargetInterfaceThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => ReflectionHelpers.GetPathPrefixFor(null));
+        }
+
+        [Fact]
+        public void TargetInterfaceHasPathPrefixAttributeReturnsCorrectPathPrefix()
+        {
+            var pathPrefix = ReflectionHelpers.GetPathPrefixFor(typeof(IInterfaceWithPathPrefix));
+            Assert.Equal("/pathPrefix", pathPrefix);
+        }
+
+        [Fact]
+        public void InheritedInterfaceHasPathPrefixAttributeReturnsCorrectPathPrefix()
+        {
+            var pathPrefix = ReflectionHelpers.GetPathPrefixFor(typeof(IInterfaceInheritingPathPrefix));
+            Assert.Equal("/pathPrefix", pathPrefix);
+        }
+
+        [Fact]
+        public void NoPathPrefixAttributeReturnsEmptyString()
+        {
+            var pathPrefix = ReflectionHelpers.GetPathPrefixFor(typeof(IInterfaceWithoutPathPrefix));
+            Assert.Equal(string.Empty, pathPrefix);
+        }
+
+        [PathPrefix("/pathPrefix")]
+        public interface IInterfaceWithPathPrefix { }
+
+        public interface IInterfaceInheritingPathPrefix : IInterfaceWithPathPrefix { }
+
+        public interface IInterfaceWithoutPathPrefix { }
+    }
+}
