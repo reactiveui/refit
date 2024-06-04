006c3c9b5f5f691972f2fc5bf7e251f6c0c83492
tcortega <tarcisiocaetano2009@gmail.com>
2024-06-04 02:08:51 -0300

    feat(tests): CamelCaseUrlParameterKeyFormatter tests

--- Refit.Tests/CamelCaseUrlParameterKeyFormatter.cs
+++ Refit.Tests/CamelCaseUrlParameterKeyFormatter.cs
@@ -1,6 +1,42 @@
-﻿namespace Refit.Tests;
+﻿using Xunit;
 
-public class CamelCaseUrlParameterKeyFormatterTest
+namespace Refit.Tests;
+
+public class CamelCaselTestsRequest
 {
+    public string alreadyCamelCased { get; set; }
+    public string NOTCAMELCased { get; set; }
+}
 
+public class CamelCaseUrlParameterKeyFormatterTests
+{
+    [Fact]
+    public void Format_EmptyKey_ReturnsEmptyKey()
+    {
+        var urlParameterKeyFormatter = new CamelCaseUrlParameterKeyFormatter();
+
+        var output = urlParameterKeyFormatter.Format(string.Empty);
+        Assert.Equal(string.Empty, output);
+    }
+
+    [Fact]
+    public void FormatKey_Returns_ExpectedValue()
+    {
+        var urlParameterKeyFormatter = new CamelCaseUrlParameterKeyFormatter();
+
+        var refitSettings = new RefitSettings { UrlParameterKeyFormatter = urlParameterKeyFormatter };
+        var fixture = new RequestBuilderImplementation<IDummyHttpApi>(refitSettings);
+        var factory = fixture.BuildRequestFactoryForMethod(nameof(IDummyHttpApi.ComplexQueryObjectWithDictionary));
+
+        var complexQuery = new CamelCaselTestsRequest
+        {
+            alreadyCamelCased = "value1",
+            NOTCAMELCased = "value2"
+        };
+
+        var output = factory([complexQuery]);
+        var uri = new Uri(new Uri("http://api"), output.RequestUri);
+
+        Assert.Equal("/foo?alreadyCamelCased=value1&notcamelCased=value2", uri.PathAndQuery);
+    }
 }
