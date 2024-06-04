691337065ea73c86a205e92dee018a5088f309e9
tcortega <tarcisiocaetano2009@gmail.com>
2024-06-04 02:09:05 -0300

    feat(Tests): RefitSettings tests

--- Refit.Tests/RefitSettings.cs
+++ Refit.Tests/RefitSettings.cs
@@ -1,6 +1,30 @@
-﻿namespace Refit.Tests;
+﻿using Xunit;
 
-public class RefitSettings
+namespace Refit.Tests;
+
+public class RefitSettingsTests
 {
-    
+    [Fact]
+    public void Can_CreateRefitSettings_WithoutException()
+    {
+        var contentSerializer = new NewtonsoftJsonContentSerializer();
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        var urlParameterKeyFormatter = new CamelCaseUrlParameterKeyFormatter();
+        var formUrlEncodedParameterFormatter = new DefaultFormUrlEncodedParameterFormatter();
+
+        var exception = Record.Exception(() => new RefitSettings());
+        Assert.Null(exception);
+
+        exception = Record.Exception(() => new RefitSettings(contentSerializer));
+        Assert.Null(exception);
+
+        exception = Record.Exception(() => new RefitSettings(contentSerializer, urlParameterFormatter));
+        Assert.Null(exception);
+
+        exception = Record.Exception(() => new RefitSettings(contentSerializer, urlParameterFormatter, formUrlEncodedParameterFormatter));
+        Assert.Null(exception);
+
+        exception = Record.Exception(() => new RefitSettings(contentSerializer, urlParameterFormatter, formUrlEncodedParameterFormatter, urlParameterKeyFormatter));
+        Assert.Null(exception);
+    }
 }
