b944483bd9ea3a0a01511f5cc9ac40d8b3011081
老九 <366193849@qq.com>
2024-06-09 23:52:42 +0800

    .NET7.0+ AOT supported (#1710)
    * .NET7.0+ AOT supported
    
    * Update unit tests.
    
    ---------
    
    Co-authored-by: Chris Pulman <chris.pulman@yahoo.com>

--- InterfaceStubGenerator.Shared/InterfaceStubGenerator.cs
+++ InterfaceStubGenerator.Shared/InterfaceStubGenerator.cs
@@ -247,6 +247,13 @@ namespace Refit.Implementation
     [global::System.ComponentModel.EditorBrowsable(global::System.ComponentModel.EditorBrowsableState.Never)]
     internal static partial class Generated
     {{
+#if NET5_0_OR_GREATER
+        [System.Runtime.CompilerServices.ModuleInitializer]
+        [System.Diagnostics.CodeAnalysis.DynamicDependency(System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.All, typeof(global::Refit.Implementation.Generated))]
+        public static void Initialize()
+        {{
+        }}
+#endif
     }}
 }}
 #pragma warning restore
