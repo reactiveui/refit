8120a95502781bf74db935112c981946fbb55460
Timothy Makkison <timmakkison@gmail.com>
2024-06-23 12:20:30 +0100

    feat: add performance and startup benchmarks

---
+++ Refit.Benchmarks/PerformanceBenchmark.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using BenchmarkDotNet.Attributes;
+
+namespace Refit.Benchmarks;
+
+[MemoryDiagnoser]
+public class PerformanceBenchmark
+{
+    private IPerformanceApi? service;
+
+    private const string Host = "https://github.com";
+    private SystemTextJsonContentSerializer systemTextJsonContentSerializer;
+
+    [GlobalSetup]
+    public Task SetupAsync()
+    {
+        systemTextJsonContentSerializer = new SystemTextJsonContentSerializer();
+        service =
+            RestService.For<IPerformanceApi>(
+                Host,
+                new RefitSettings(systemTextJsonContentSerializer)
+                {
+                    HttpMessageHandlerFactory = () =>
+                        new StaticValueHttpResponseHandler(
+                            "Ok",
+                            HttpStatusCode.OK
+                        )
+                }
+            );
+
+        return Task.CompletedTask;
+    }
+
+    [Benchmark]
+    public async Task<string> ConstantRouteAsync() => await service.ConstantRoute();
+
+    [Benchmark]
+    public async Task<string> DynamicRouteAsync() => await service.DynamicRoute(101);
+
+    [Benchmark]
+    public async Task<string> ComplexDynamicRouteAsync() => await service.ComplexDynamicRoute(101, "tom", "yCxv");
+
+    [Benchmark]
+    public async Task<string> ObjectRequestAsync() => await service.ObjectRequest(new PathBoundObject(){SomeProperty = "myProperty", SomeQuery = "myQuery"});
+
+    [Benchmark]
+    public async Task<string> ComplexRequestAsync() => await service.ComplexRequest(101, new PathBoundObject(){SomeProperty = "myProperty", SomeQuery = "myQuery"}, [1,2,3,4,5,6]);
+}
---
+++ Refit.Benchmarks/StartupBenchmark.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using BenchmarkDotNet.Attributes;
+
+namespace Refit.Benchmarks;
+
+[MemoryDiagnoser]
+public class StartupBenchmark
+{
+    private const string Host = "https://github.com";
+    private readonly RefitSettings settings = new RefitSettings()
+    {
+        HttpMessageHandlerFactory = () =>
+            new StaticValueHttpResponseHandler(
+                "Ok",
+                HttpStatusCode.OK
+            )
+    };
+
+    [Benchmark]
+    public IPerformanceApi CreateService() => RestService.For<IPerformanceApi>(Host, settings);
+
+    [Benchmark]
+    public async Task<string> ConstantRouteAsync()
+    {
+        var service = RestService.For<IPerformanceApi>(Host, settings);
+        return await service.ConstantRoute();
+    }
+
+
+    [Benchmark]
+    public async Task<string> ComplexRequestAsync()
+    {
+        var service = RestService.For<IPerformanceApi>(Host, settings);
+        return await service.ObjectRequest(new PathBoundObject(){SomeProperty = "myProperty", SomeQuery = "myQuery"});
+    }
+}
