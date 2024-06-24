8a40692db0322c49ee82ca0dac2e733c46de496f
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2024-06-24 19:48:28 +0100

    feat: add startup and performance benchmarks (#1731)
    * chore: use file scoped namespaces
    
    * feat: add performance and startup benchmarks
    
    * feat: add first call benchmarks

---
+++ Refit.Benchmarks/StartupBenchmark.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using BenchmarkDotNet.Attributes;
+
+namespace Refit.Benchmarks;
+
+[MemoryDiagnoser]
+public class StartupBenchmark
+{
+    private IPerformanceService initialisedService;
+    private const string Host = "https://github.com";
+    private readonly RefitSettings settings = new RefitSettings()
+    {
+        HttpMessageHandlerFactory = () =>
+            new StaticValueHttpResponseHandler(
+                "Ok",
+                HttpStatusCode.OK
+            )
+    };
+
+
+    [IterationSetup(Targets = [nameof(FirstCallConstantRouteAsync), nameof(FirstCallComplexRequestAsync)])]
+    public void Setup()
+    {
+        initialisedService = RestService.For<IPerformanceService>(Host, settings);
+    }
+
+    [Benchmark]
+    public IPerformanceService CreateService() => RestService.For<IPerformanceService>(Host, settings);
+
+    [Benchmark]
+    public async Task<string> FirstCallConstantRouteAsync() => await initialisedService.ConstantRoute();
+
+    [Benchmark]
+    public async Task<string> ConstantRouteAsync()
+    {
+        var service = RestService.For<IPerformanceService>(Host, settings);
+        return await service.ConstantRoute();
+    }
+
+    [Benchmark]
+    public async Task<string> FirstCallComplexRequestAsync() => await initialisedService.ObjectRequest(new PathBoundObject(){SomeProperty = "myProperty", SomeQuery = "myQuery"});
+
+    [Benchmark]
+    public async Task<string> ComplexRequestAsync()
+    {
+        var service = RestService.For<IPerformanceService>(Host, settings);
+        return await service.ObjectRequest(new PathBoundObject(){SomeProperty = "myProperty", SomeQuery = "myQuery"});
+    }
+}
---
+++ Refit.Benchmarks/StaticValueHttpResponseHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+
+namespace Refit.Benchmarks;
+
+public class StaticValueHttpResponseHandler (string response, HttpStatusCode code) : HttpMessageHandler
+{
+    protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request,
+        CancellationToken cancellationToken
+    )
+    {
+            return Task.FromResult(
+                new HttpResponseMessage(code)
+                {
+                    RequestMessage = request,
+                    Content = new StringContent(response)
+                }
+            );
+        }
+}
