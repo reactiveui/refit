03d7bbc7abae4e0e6440f635223cf6f6f64fb922
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2024-06-25 19:36:50 +0100

    feat: use `TryGetSingle` instead of collection enumerable to lists. (#1738)
    Co-authored-by: Chris Pulman <chris.pulman@yahoo.com>

---
+++ Refit/EnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Refit;
+
+internal static class EnumerableExtensions
+{
+    internal static EnumerablePeek TryGetSingle<T>(this IEnumerable<T> enumerable, out T? value)
+    {
+        value = default(T);
+        using var enumerator = enumerable.GetEnumerator();
+        var hasFirst = enumerator.MoveNext();
+        if (!hasFirst)
+            return EnumerablePeek.Empty;
+
+        value = enumerator.Current;
+        if (!enumerator.MoveNext())
+            return EnumerablePeek.Single;
+
+        value = default(T);
+        return EnumerablePeek.Many;
+    }
+}
+
+internal enum EnumerablePeek
+{
+    Empty,
+    Single,
+    Many
+}
