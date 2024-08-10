1d68d1d58fa0093fe136a56ad74ba39c0841fee7
Pavel Kravtsov <kravtsovpo@gmail.com>
2024-08-10 18:24:15 +0700

    Add DefaultUrlParameterFormatterTests

---
+++ Refit.Tests/DefaultUrlParameterFormatterTests.cs
@@ -0,0 +1,188 @@
+﻿using System.Globalization;
+using System.Reflection;
+using Xunit;
+
+namespace Refit.Tests;
+
+public class DefaultUrlParameterFormatterTests
+{
+    class DateTimeRequestWithQueryAttribute
+    {
+        [Query(Format = "yyyy")] public DateTime? DateTimeYear { get; set; }
+    }
+
+    class DateTimeRequestWithoutQueryAttribute
+    {
+        public DateTime DateTime { get; set; }
+    }
+
+    [Fact]
+    public void NullParameterValue_ReturnsNull()
+    {
+        var parameters = new DateTimeRequestWithQueryAttribute
+        {
+            DateTimeYear = null
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTimeYear,
+            parameters.GetType().GetProperty(nameof(parameters.DateTimeYear))!,
+            parameters.GetType());
+
+        Assert.Null(output);
+    }
+
+    [Fact]
+    public void NoFormatters_UseDefaultFormat()
+    {
+        var parameters = new DateTimeRequestWithoutQueryAttribute
+        {
+            DateTime = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTime,
+            parameters.GetType().GetProperty(nameof(parameters.DateTime))!,
+            parameters.GetType());
+
+        Assert.Equal("08/21/2023 00:00:00", output);
+    }
+
+    [Fact]
+    public void QueryAttributeFormatOnly_FormattedByQueryAttribute()
+    {
+        var parameters = new DateTimeRequestWithQueryAttribute
+        {
+            DateTimeYear = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTimeYear,
+            parameters.GetType().GetProperty(nameof(parameters.DateTimeYear))!,
+            parameters.GetType());
+
+        Assert.Equal("2023", output);
+    }
+
+    [Fact]
+    public void QueryAttributeAndGeneralFormats_FormattedByQueryAttribute()
+    {
+        var parameters = new DateTimeRequestWithQueryAttribute
+        {
+            DateTimeYear = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy-MM-dd");
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTimeYear,
+            parameters.GetType().GetProperty(nameof(parameters.DateTimeYear))!,
+            parameters.GetType());
+
+        Assert.Equal("2023", output);
+    }
+
+    [Fact]
+    public void QueryAttributeAndSpecificFormats_FormattedByQueryAttribute()
+    {
+        var parameters = new DateTimeRequestWithQueryAttribute
+        {
+            DateTimeYear = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTimeRequestWithQueryAttribute, DateTime>("yyyy-MM-dd");
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTimeYear,
+            parameters.GetType().GetProperty(nameof(parameters.DateTimeYear))!,
+            parameters.GetType());
+
+        Assert.Equal("2023", output);
+    }
+
+    [Fact]
+    public void AllFormats_FormattedByQueryAttribute()
+    {
+        var parameters = new DateTimeRequestWithQueryAttribute
+        {
+            DateTimeYear = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy-MM-dd");
+        urlParameterFormatter.AddFormat<DateTimeRequestWithoutQueryAttribute, DateTime>("yyyy-MM-dd");
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTimeYear,
+            parameters.GetType().GetProperty(nameof(parameters.DateTimeYear))!,
+            parameters.GetType());
+
+        Assert.Equal("2023", output);
+    }
+
+    [Fact]
+    public void GeneralFormatOnly_FormattedByGeneralFormat()
+    {
+        var parameters = new DateTimeRequestWithoutQueryAttribute
+        {
+            DateTime = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy");
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTime,
+            parameters.GetType().GetProperty(nameof(parameters.DateTime))!,
+            parameters.GetType());
+
+        Assert.Equal("2023", output);
+    }
+
+    [Fact]
+    public void SpecificFormatOnly_FormattedBySpecificFormat()
+    {
+        var parameters = new DateTimeRequestWithoutQueryAttribute
+        {
+            DateTime = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTimeRequestWithoutQueryAttribute, DateTime>("yyyy");
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTime,
+            parameters.GetType().GetProperty(nameof(parameters.DateTime))!,
+            parameters.GetType());
+
+        Assert.Equal("2023", output);
+    }
+
+    [Fact]
+    public void GeneralAndSpecificFormats_FormattedBySpecificFormat()
+    {
+        var parameters = new DateTimeRequestWithoutQueryAttribute
+        {
+            DateTime = new DateTime(2023, 8, 21)
+        };
+
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy-MM-dd");
+        urlParameterFormatter.AddFormat<DateTimeRequestWithoutQueryAttribute, DateTime>("yyyy");
+
+        var output = urlParameterFormatter.Format(
+            parameters.DateTime,
+            parameters.GetType().GetProperty(nameof(parameters.DateTime))!,
+            parameters.GetType());
+
+        Assert.Equal("2023", output);
+    }
+}
