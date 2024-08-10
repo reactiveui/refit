29d874aec3af986cd2efe4f95666ec6fcca48056
Pavel Kravtsov <kravtsovpo@gmail.com>
2024-08-10 14:38:24 +0700

    Extend URL parameters formatting through DefaultUrlParameterFormatter

--- Refit/RefitSettings.cs
+++ Refit/RefitSettings.cs
@@ -185,7 +185,7 @@ namespace Refit
         /// </summary>
         /// <param name="value">The value.</param>
         /// <param name="attributeProvider">The attribute provider.</param>
-        /// <param name="type">The type.</param>
+        /// <param name="type">Container class type.</param>
         /// <returns></returns>
         string? Format(object? value, ICustomAttributeProvider attributeProvider, Type type);
     }
@@ -227,12 +227,39 @@ namespace Refit
             ConcurrentDictionary<string, EnumMemberAttribute?>
         > EnumMemberCache = new();
 
+        Dictionary<(Type containerType, Type parameterType), string> SpecificFormats { get; } = new();
+
+        Dictionary<Type, string> GeneralFormats { get; } = new();
+
+        /// <summary>
+        /// Add format for specified parameter type contained within container class of specified type.
+        /// Might be suppressed by a QueryAttribute format.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <typeparam name="TContainer">Container class type.</typeparam>
+        /// <typeparam name="TParameter">Parameter type.</typeparam>
+        public void AddFormat<TContainer, TParameter>(string format)
+        {
+            SpecificFormats.Add((typeof(TContainer), typeof(TParameter)), format);
+        }
+
+        /// <summary>
+        /// Add format for specified parameter type.
+        /// Might be suppressed by a QueryAttribute format or a container specific format.
+        /// </summary>
+        /// <param name="format">The format string.</param>
+        /// <typeparam name="TParameter">Parameter type.</typeparam>
+        public void AddFormat<TParameter>(string format)
+        {
+            GeneralFormats.Add(typeof(TParameter), format);
+        }
+
         /// <summary>
         /// Formats the specified parameter value.
         /// </summary>
         /// <param name="parameterValue">The parameter value.</param>
         /// <param name="attributeProvider">The attribute provider.</param>
-        /// <param name="type">The type.</param>
+        /// <param name="type">Container class type.</param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException">attributeProvider</exception>
         public virtual string? Format(
@@ -274,6 +301,18 @@ namespace Refit
                             .First()
                             .GetCustomAttribute<EnumMemberAttribute>()
                 );
+            }
+
+            if (string.IsNullOrWhiteSpace(formatString) &&
+                SpecificFormats.TryGetValue((type, parameterType), out var specificFormat))
+            {
+                formatString = specificFormat;
+            }
+
+            if (string.IsNullOrWhiteSpace(formatString) &&
+                GeneralFormats.TryGetValue(parameterType, out var generalFormat))
+            {
+                formatString = generalFormat;
             }
 
             return string.Format(
