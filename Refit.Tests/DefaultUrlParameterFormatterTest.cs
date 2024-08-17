fd9c4ddc9866f43c4ab5d8ce14f865d811e17ebd
Pavel Kravtsov <kravtsovpo@gmail.com>
2024-08-17 14:54:19 +0700

    Add DefaultUrlParameterFormatter tests with URI building

--- Refit.Tests/DefaultUrlParameterFormatterTest.cs
+++ Refit.Tests/DefaultUrlParameterFormatterTest.cs
@@ -11,6 +11,12 @@ public class DefaultUrlParameterFormatterTests
         [Query(Format = "yyyy")] public DateTime? DateTimeWithAttributeFormatYear { get; set; }
 
         public DateTime? DateTime { get; set; }
+
+        public IEnumerable<DateTime> DateTimeCollection { get; set; }
+
+        public IDictionary<int, DateTime> DateTimeDictionary { get; set; }
+
+        public IDictionary<DateTime, int> DateTimeKeyedDictionary { get; set; }
     }
 
     [Fact]
@@ -181,5 +187,117 @@ public class DefaultUrlParameterFormatterTests
             parameters.GetType());
 
         Assert.Equal("2023", output);
+    }
+
+    [Fact]
+    public void RequestWithPlainDateTimeQueryParameter_ProducesCorrectQueryString()
+    {
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy");
+
+        var refitSettings = new RefitSettings { UrlParameterFormatter = urlParameterFormatter };
+        var fixture = new RequestBuilderImplementation<IDummyHttpApi>(refitSettings);
+        var factory = fixture.BuildRequestFactoryForMethod(
+            nameof(IDummyHttpApi.PostWithComplexTypeQuery)
+        );
+
+        var parameters = new DefaultUrlParameterFormatterTestRequest
+        {
+            DateTime = new DateTime(2023, 8, 21),
+        };
+
+        var output = factory([parameters]);
+        var uri = new Uri(new Uri("http://api"), output.RequestUri);
+
+        Assert.Equal(
+            "?DateTime=2023",
+            uri.Query
+        );
+    }
+
+    [Fact]
+    public void RequestWithDateTimeCollectionQueryParameter_ProducesCorrectQueryString()
+    {
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy");
+
+        var refitSettings = new RefitSettings { UrlParameterFormatter = urlParameterFormatter };
+        var fixture = new RequestBuilderImplementation<IDummyHttpApi>(refitSettings);
+        var factory = fixture.BuildRequestFactoryForMethod(
+            nameof(IDummyHttpApi.PostWithComplexTypeQuery)
+        );
+
+        var parameters = new DefaultUrlParameterFormatterTestRequest
+        {
+            DateTimeCollection = [new DateTime(2023, 8, 21), new DateTime(2024, 8, 21)],
+        };
+
+        var output = factory([parameters]);
+        var uri = new Uri(new Uri("http://api"), output.RequestUri);
+
+        Assert.Equal(
+            "?DateTimeCollection=2023%2C2024",
+            uri.Query
+        );
+    }
+
+    [Fact]
+    public void RequestWithDateTimeDictionaryQueryParameter_ProducesCorrectQueryString()
+    {
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy");
+
+        var refitSettings = new RefitSettings { UrlParameterFormatter = urlParameterFormatter };
+        var fixture = new RequestBuilderImplementation<IDummyHttpApi>(refitSettings);
+        var factory = fixture.BuildRequestFactoryForMethod(
+            nameof(IDummyHttpApi.PostWithComplexTypeQuery)
+        );
+
+        var parameters = new DefaultUrlParameterFormatterTestRequest
+        {
+            DateTimeDictionary = new Dictionary<int, DateTime>
+            {
+                { 1, new DateTime(2023, 8, 21) },
+                { 2, new DateTime(2024, 8, 21) },
+            },
+        };
+
+        var output = factory([parameters]);
+        var uri = new Uri(new Uri("http://api"), output.RequestUri);
+
+        Assert.Equal(
+            "?DateTimeDictionary.1=2023&DateTimeDictionary.2=2024",
+            uri.Query
+        );
+    }
+
+    [Fact]
+    public void RequestWithDateTimeKeyedDictionaryQueryParameter_ProducesCorrectQueryString()
+    {
+        var urlParameterFormatter = new DefaultUrlParameterFormatter();
+        urlParameterFormatter.AddFormat<DateTime>("yyyy");
+
+        var refitSettings = new RefitSettings { UrlParameterFormatter = urlParameterFormatter };
+        var fixture = new RequestBuilderImplementation<IDummyHttpApi>(refitSettings);
+        var factory = fixture.BuildRequestFactoryForMethod(
+            nameof(IDummyHttpApi.PostWithComplexTypeQuery)
+        );
+
+        var parameters = new DefaultUrlParameterFormatterTestRequest
+        {
+            DateTimeKeyedDictionary = new Dictionary<DateTime, int>
+            {
+                { new DateTime(2023, 8, 21), 1 },
+                { new DateTime(2024, 8, 21), 2 },
+            },
+        };
+
+        var output = factory([parameters]);
+        var uri = new Uri(new Uri("http://api"), output.RequestUri);
+
+        Assert.Equal(
+            "?DateTimeKeyedDictionary.2023=1&DateTimeKeyedDictionary.2024=2",
+            uri.Query
+        );
     }
 }
