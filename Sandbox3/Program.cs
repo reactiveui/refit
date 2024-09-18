e8e0221e9b8f6ba3d6798ea56970d8cb14e5559f
Timothy Makkison <timmakkison@gmail.com>
2024-09-18 21:17:29 +0100

    chore: my sandbox test

---
+++ Sandbox3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Refit;
+
+var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com");
+Console.WriteLine("Success?");
+
+public interface IGitHubApi
+{
+    [Get("/users/{user}")]
+    Task<User> GetUser(string user);
+}
+
+public record User();
