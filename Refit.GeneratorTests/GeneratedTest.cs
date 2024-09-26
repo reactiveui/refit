a1df780bc7d3ce0bab1259230d92810755343db5
Timothy Makkison <timmakkison@gmail.com>
2024-09-26 14:40:48 +0100

    feat: add `ShouldNotEmitFiles` test

--- Refit.GeneratorTests/GeneratedTest.cs
+++ Refit.GeneratorTests/GeneratedTest.cs
@@ -11,4 +11,11 @@ public class GeneratedTest
             Task<string> Get();
             """, false);
     }
+
+    [Fact]
+    public Task ShouldNotEmitFilesWhenNoRefitInterfaces()
+    {
+        // Refit should not generate any code when no valid Refit interfaces are present.
+        return Fixture.VerifyForBody("", false);
+    }
 }
