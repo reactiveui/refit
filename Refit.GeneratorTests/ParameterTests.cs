606a6c6196e07486d5c61032d4220eccd0f269f3
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2024-10-06 20:35:46 +0100

    feat: added nullable and parameter tests (#1863)
    Co-authored-by: Chris Pulman <chris.pulman@yahoo.com>

---
+++ Refit.GeneratorTests/ParameterTests.cs
@@ -0,0 +1,44 @@
+﻿namespace Refit.GeneratorTests;
+
+public class ParameterTests
+{
+    [Fact]
+    public Task RouteParameter()
+    {
+        return Fixture.VerifyForBody(
+            """
+            [Get("/users/{user}")]
+            Task<string> Get(string user);
+            """);
+    }
+
+    [Fact]
+    public Task NullableRouteParameter()
+    {
+        return Fixture.VerifyForBody(
+            """
+            [Get("/users/{user}")]
+            Task<string> Get(string? user);
+            """);
+    }
+
+    [Fact]
+    public Task ValueTypeRouteParameter()
+    {
+        return Fixture.VerifyForBody(
+            """
+            [Get("/users/{user}")]
+            Task<string> Get(int user);
+            """);
+    }
+
+    [Fact]
+    public Task NullableValueTypeRouteParameter()
+    {
+        return Fixture.VerifyForBody(
+            """
+            [Get("/users/{user}")]
+            Task<string> Get(int? user);
+            """);
+    }
+}
