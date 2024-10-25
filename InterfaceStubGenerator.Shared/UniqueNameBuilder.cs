4d6f4a6edf48389a3b4d83f36c27ba979d98ae85
Timothy Makkison <timmakkison@gmail.com>
2024-10-25 23:35:31 +0100

    feat: add `UniqueNameBuilder`

---
+++ InterfaceStubGenerator.Shared/UniqueNameBuilder.cs
@@ -0,0 +1,51 @@
+﻿namespace Refit.Generator;
+
+public class UniqueNameBuilder()
+{
+    private readonly HashSet<string> _usedNames = new(StringComparer.Ordinal);
+    private readonly UniqueNameBuilder? _parentScope;
+
+    private UniqueNameBuilder(UniqueNameBuilder parentScope)
+        : this()
+    {
+        _parentScope = parentScope;
+    }
+
+    public void Reserve(string name) => _usedNames.Add(name);
+
+    public UniqueNameBuilder NewScope() => new(this);
+
+    public string New(string name)
+    {
+        var i = 0;
+        var uniqueName = name;
+        while (Contains(uniqueName))
+        {
+            uniqueName = name + i;
+            i++;
+        }
+
+        _usedNames.Add(uniqueName);
+
+        return uniqueName;
+    }
+
+    public void Reserve(IEnumerable<string> names)
+    {
+        foreach (var name in names)
+        {
+            _usedNames.Add(name);
+        }
+    }
+
+    private bool Contains(string name)
+    {
+        if (_usedNames.Contains(name))
+            return true;
+
+        if (_parentScope != null)
+            return _parentScope.Contains(name);
+
+        return false;
+    }
+}
