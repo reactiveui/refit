a7e8505674059775c92a62f5585e04f46dbc8326
Timothy Makkison <timmakkison@gmail.com>
2025-04-10 22:21:04 +0100

    feat: add aot sandbox

---
+++ Refit.AotSandbox/AotContentSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Refit;
+
+public class AotContentSerializer(JsonSerializerContext serializerContext) : IHttpContentSerializer
+{
+    public HttpContent ToHttpContent<T>(T item) => throw new NotImplementedException();
+
+    public async Task<T?> FromHttpContentAsync<T>(
+        HttpContent content,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var val = await content.ReadAsStringAsync().ConfigureAwait(false);
+
+        return (T)JsonSerializer.Deserialize(val, typeof(T), serializerContext);
+    }
+
+    public string? GetFieldNameForProperty(PropertyInfo propertyInfo) =>
+        throw new NotImplementedException();
+}
---
+++ Refit.AotSandbox/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+using Refit;
+
+// Note: this is for testing purposes, Refit does not currently support AOT.
+var settings = new RefitSettings()
+{
+    ContentSerializer = new AotContentSerializer(SandboxJsonSerializerContext.Default),
+};
+
+var gitHubApi = RestService.For<IGitHubApi>("https://api.github.com", settings);
+var octocat = await gitHubApi.GetUser("octocat");
+Console.WriteLine(octocat);
+
+Console.ReadLine();
+
+public interface IGitHubApi
+{
+    [Get("/users/{user}")]
+    [Headers("User-Agent: Refit")]
+    Task<User> GetUser(string user);
+}
+
+public record User
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("blog")]
+    public string Blog { get; set; }
+}
+
+[JsonSerializable(typeof(User))]
+partial class SandboxJsonSerializerContext : JsonSerializerContext;
