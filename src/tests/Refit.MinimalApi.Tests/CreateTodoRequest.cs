5a8c51e54125d1b2a8b86980a8953b7958beeece
Glenn Watson <5834289+glennawatson@users.noreply.github.com>
2026-06-21 06:47:18 +1000

    Remove Minimal API package

--- src/tests/Refit.MinimalApi.Tests/CreateTodoRequest.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2019-2026 ReactiveUI and Contributors. All rights reserved.
-// ReactiveUI and Contributors licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for full license information.
-
-namespace Refit.MinimalApi.Tests;
-
-/// <summary>A create request accepted by the test API.</summary>
-/// <param name="Title">The item title.</param>
-internal sealed record CreateTodoRequest(string Title);
