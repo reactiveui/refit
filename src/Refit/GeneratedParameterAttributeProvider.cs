b575c9d95c6101d5b043188075ffdef8d3f7bc50
Tim M <49349513+TimothyMakkison@users.noreply.github.com>
2026-07-13 14:12:05 +0100

    perf: `AllAttributesCache` replace `Lazy` with laziliy initialised code (#2211)

--- src/Refit/GeneratedParameterAttributeProvider.cs
+++ src/Refit/GeneratedParameterAttributeProvider.cs
@@ -2,6 +2,7 @@
 // ReactiveUI and Contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
 namespace Refit;
@@ -10,20 +11,41 @@ namespace Refit;
 /// <param name="attributes">The attribute information.</param>
 public sealed class GeneratedParameterAttributeProvider(Dictionary<Type, object[]> attributes) : ICustomAttributeProvider
 {
-    /// <summary>List of all attributes.</summary>
-    private readonly Lazy<object[]> _allAttributesCache = new(() =>
+    /// <summary>Gets a lazily initialised array of all attributes.</summary>
+    private object[] AllAttributesCache
     {
-        var allAttributes = new List<object>();
-        foreach (var value in attributes.Values)
+        [SuppressMessage("StyleCop.Analyzers", "SST1443:ReduceNestedFlowComplexity", Justification = "The nested logic is necessary here to avoid LINQ usage.")]
+        get
         {
-            allAttributes.AddRange(value);
-        }
+            if (field is not null)
+            {
+                return field;
+            }
 
-        return allAttributes.ToArray();
-    });
+            var totalCount = 0;
+            foreach (var attributeArray in attributes.Values)
+            {
+                totalCount += attributeArray.Length;
+            }
+
+            var allAttributes = new object[totalCount];
+            var index = 0;
+            foreach (var attributeArray in attributes.Values)
+            {
+                foreach (var item in attributeArray)
+                {
+                    allAttributes[index++] = item;
+                }
+            }
+
+            _ = Interlocked.CompareExchange(ref field, allAttributes, null);
+
+            return field;
+        }
+    }
 
     /// <inheritdoc/>
-    public object[] GetCustomAttributes(bool inherit) => _allAttributesCache.Value;
+    public object[] GetCustomAttributes(bool inherit) => AllAttributesCache;
 
     /// <inheritdoc/>
     public object[] GetCustomAttributes(Type attributeType, bool inherit)
